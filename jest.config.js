c8aeac1f63b2d6da4970b5aaa8924ed9fedf57c9
Aleksandar Cakic <acakic92@gmail.com>
2021-08-30 00:39:23 -0500

    Add Codecov

--- jest.config.js
+++ jest.config.js
@@ -95,7 +95,8 @@ module.exports = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  // preset: 'ts-jest',
+
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -144,7 +145,8 @@ module.exports = {
   // ],
 
   // The test environment that will be used for testing
-  testEnvironment: 'jsdom', //'enzyme', //'jsdom',
+  testEnvironment: 'node', //'enzyme', //'jsdom',
+
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
