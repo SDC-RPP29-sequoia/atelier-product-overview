6e62a73153048c4e47a69892e658ea1436ffa35f
Aleksandar Cakic <acakic92@gmail.com>
2021-08-30 01:11:24 -0500

    Update fail test

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -8,6 +8,6 @@ test('adds 1 + 4 to equal 5', () => {
   expect(sum(1, 4)).toBe(5);
 });
 
-test('adds 1 + 5 to equal 7', () => {
-  expect(sum(1, 5)).toBe(7);
+test('adds 1 + 5 to equal 6', () => {
+  expect(sum(1, 5)).toBe(6);
 });
