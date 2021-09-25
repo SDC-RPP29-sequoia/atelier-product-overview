f27227812f228c30f4cc26be742631cee2263398
Aleksandar Cakic <acakic92@gmail.com>
2021-09-24 19:25:32 -0500

    Import all files to mongodb

--- server/db/mongo/index.js
+++ server/db/mongo/index.js
@@ -12,39 +12,3 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTo
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('connected', () => console.log('Mongoose Connected'));
-
-
-// let database = 'sdc_product_overview';
-// let collection = 'styles';
-
-// for (let i = 8; i < 40; i++) {
-
-
-//   let exec = require('child_process').exec
-//   let command = `mongoimport -db ${database} -collection ${collection} --type json --jsonArray --file docs/data/json/styleObject/${i}.json`
-//   child_process.execSync(command)
-// }
-
-// Product.find({name: 'Camo Onesie'}).exec().then(function (doc) {
-//   console.log('Doc', doc[0])
-// })
-
-// mongoimport --db sdc_product_overview --collection styles --drop --type json --file docs/data/json/styleObject/0.json --jsonArray
-// mongoimport --db sdc_product_overview --collection products --drop --type json --file docs/data/json/0.json --jsonArray
-
-// Drop database
-// db.dropDatabase();
-// Add 2 collections
-// Add docs to each collection
-
-// Get style data with photos and skus
-
-// Product.find({name: 'Camo Onesie'})
-//   .populate('Feature')
-//   .exec(function (err, result) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log(result);
-//     }
-//   });
